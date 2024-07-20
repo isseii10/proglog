05033853afdacfbd01eda50fa9cf9213f3f49596
isseii10 <t1l.g2.o11g@gmail.com>
2024-07-20 19:56:52 +0900

    fix

--- internal/log/store.go
+++ internal/log/store.go
@@ -13,81 +13,81 @@ const lenWidth = 8
 
 // store: ログを保存するファイル
 type store struct {
-  *os.File
-  mu sync.Mutex
-  buf *bufio.Writer
-  size uint64
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
 }
 
 func newStore(f *os.File) (*store, error) {
-  fi, err := os.Stat(f.Name())
-  if err != nil {
-    return nil, err
-  }
-  size := uint64(fi.Size())
-  return &store{
-    File: f,
-    size: size,
-    buf: bufio.NewWriter(f),
-  }, nil
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
 }
 
 func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
-  s.mu.Lock()
-  defer s.mu.Unlock()
-  pos = s.size
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
 
 	// binary.Writeで書き込む長さを先に書き込む
-  if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
-    return 0, 0, err
-  }
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
 	// 中身を書き込む
-  w, err := s.buf.Write(p)
-  if err != nil {
-    return 0, 0, err
-  }
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
 	// 長さも書き込んだのでlenWidth足す
-  w += lenWidth
+	w += lenWidth
 	// storeのsizeを更新
-  s.size += uint64(w)
-  return uint64(w), pos, nil
+	s.size += uint64(w)
+	return uint64(w), pos, nil
 }
 
 func (s *store) Read(pos uint64) ([]byte, error) {
-  s.mu.Lock()
-  defer s.mu.Unlock()
-  if err := s.buf.Flush(); err != nil {
-    return nil, err
-  }
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
 	// まずはデータの長さ(size)を読み取る
-  size := make([]byte, lenWidth)
-  if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
-    return nil, err
-  }
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
 	// size分読み取る。実際にはposからlenWidthだけ先にdataがあるのでpos+lenWidthをしている。
-  b := make([]byte, enc.Uint64(size))
-  if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
-    return nil, err
-  }
-  return b, nil
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
 }
 
 func (s *store) ReadAt(p []byte, off int64) (int, error) {
-  s.mu.Lock()
-  defer s.mu.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-  if err := s.buf.Flush(); err != nil {
-    return 0, err
-  }
-  return s.File.ReadAt(p, off)
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
 }
 
 func (s *store) Close() error {
-  s.mu.Lock()
-  defer s.mu.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-  if err := s.buf.Flush(); err != nil {
-    return err
-  }
-  return s.File.Close()
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+	return s.File.Close()
 }
